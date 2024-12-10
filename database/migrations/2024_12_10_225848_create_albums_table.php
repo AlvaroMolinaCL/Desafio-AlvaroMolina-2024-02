c6ae540ccb43ca6191833dbff3d0725e65e0cd2e
Alvaro Molina Jara <22986107+AlvaroMolinaCL@users.noreply.github.com>
2024-12-10 20:05:38 -0300

    Se crean migraciones para mantenedores

---
+++ database/migrations/2024_12_10_225848_create_albums_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('artist'); // Relación inicial como texto
+            $table->integer('release_year');
+            $table->string('cover')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
