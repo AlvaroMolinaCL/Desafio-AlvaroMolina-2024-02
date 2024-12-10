7e292d585cd7ada66156ae8efc3b1101bd6236fa
Alvaro Molina Jara <22986107+AlvaroMolinaCL@users.noreply.github.com>
2024-12-10 20:46:05 -0300

    Se actualizan rutas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\PlaylistController;
+use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +18,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::resource('genres', GenreController::class);
+    Route::resource('albums', AlbumController::class);
+    Route::resource('artists', ArtistController::class);
+    Route::resource('playlists', PlaylistController::class);
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/', [PublicController::class, 'index'])->name('home');
+Route::get('/search', [PublicController::class, 'search'])->name('search');
 
 require __DIR__.'/auth.php';
