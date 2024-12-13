480230c164bb648c31e6f9404a860479759b54e7
Alvaro Molina Jara <22986107+AlvaroMolinaCL@users.noreply.github.com>
2024-12-13 01:26:37 -0300

    Se mejoran estilos

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mx-auto px-4 py-6">
+    <div class="container mx-auto px-4 py-6 text-center">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 bg-white border-b border-gray-200">
-                <h1 class="text-2xl font-bold mb-4">Dashboard</h1>
+                <h1 class="text-2xl font-bold mb-4">Panel de Administración</h1>
 
                 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
                     <!-- Tarjeta de Géneros -->
